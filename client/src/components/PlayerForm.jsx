b919013699745b851717428a4eca081748bc8ccb
japhethbear <japhethbear@gmail.com>
2023-03-18 14:23:12 -0500

    added react component

---
+++ client/src/components/PlayerForm.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import axios from 'axios'
+import { useState } from 'react'
+
+export const PlayerForm = () => {
+  
+  const [player, setPlayer] = useState({
+    name: "",
+    preferredPosition: ""
+  })
+
+  const [errors, setErrors] = useState({})
+  
+  const onChangeHandler = (e) => {
+    setPlayer({
+      ...player,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const formValidator = () => {
+    let isValid = true
+    if (player.name.length <2) {
+      return false
+    }
+    if (player.preferredPosition.length < 2) {
+      return false
+    }
+    return isValid
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (formValidator()) {
+      axios.post('http://localhost:8000/api/players', player)
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+    }
+    else {
+      setErrors({
+        name: "Name must be at least 2 characters",
+        preferredPosition: "Preferred Position must be at least 2 characters"
+      })
+    }
+  }
+
+
+
+  return (
+    <div>
+      <h1>Add Player</h1>
+        {errors.name ? <p className="text-danger">{errors.name}</p> : ""}
+        {errors.preferredPosition ? <p className='text-danger'>{errors.preferredPosition}</p> : ""}
+      <form action="" className="col-md-6 mx-auto" onSubmit={handleSubmit}>
+        <div className='form-group'>
+          <label htmlFor='name'>Player Name: </label>
+          <input type="text" className='form-control' name="name" id="name" onChange={onChangeHandler}/>
+        </div>
+        <div className='form-group'>
+          <label htmlFor='preferredPosition'>Preferred Position: </label>
+          <input type="text" className='form-control' name="preferredPosition" id="preferredPosition" onChange={onChangeHandler}/>
+        </div>
+        <button className='btn btn-info'>Create Player</button>
+      </form>
+    </div>
+  )
+
+  }
+
+  export default PlayerForm
